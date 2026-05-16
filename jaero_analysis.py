9ec443ade1a3b0f1a4d022cfa8c97b45b1556717
Luca Laureno <luca.laureno@gmail.com>
2026-05-16 12:00:11 -0400

    hid data files for confidentiality

--- jaero_analysis.py
+++ jaero_analysis.py
@@ -6,3 +6,7 @@ import seaborn as sns
 from scipy import stats
 
 # creating dataframes
+jaero_logs = pd.read_csv("data/jaero_logs.csv")
+print(jaero_logs.head())
+
+# divide logs into logs with message and logs without
